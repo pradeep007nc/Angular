ced944035f3cf0d8886d22a105ea58a224497d6a
pradeep007nc <pradeepnaidu2486@gmail.com>
2023-11-06 20:58:27 +0530

    main

--- Assignment/Angular-Frontend/src/app/Entities/data-skeleton.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DataSkeleton } from './data-skeleton';
-
-describe('DataSkeleton', () => {
-  it('should create an instance', () => {
-    expect(new DataSkeleton()).toBeTruthy();
-  });
-});
--- Assignment/Angular-Frontend/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl:'http://localhost:3000',
-  name:'production environment',
-};
